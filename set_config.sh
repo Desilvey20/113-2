aa412ae25331ff90f74fb7872ef441de25f88050
Desilvey20 <101611124+Desilvey20@users.noreply.github.com>
2022-07-20 16:57:59 -0700

    Add files via upload

---
+++ set_config.sh
@@ -0,0 +1,6 @@
+
+
+heroku config:set EMAIL_HOST=smtp.sendgrid.net
+heroku config:set EMAIL_HOST_USER="apikey"
+heroku config:set EMAIL_HOST_PASSWORD="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"
+heroku config:set DEFAULT_FROM_EMAIL="desilveydavid3@gmail.com"
